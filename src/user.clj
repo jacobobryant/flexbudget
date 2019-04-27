a8d1f78be8490b6909ea34cc70be6aea07c45605
Jacob O'Bryant <jacobobryant7@gmail.com>
2019-04-26 21:49:05 -0600

    hey

---
+++ src/user.clj
@@ -0,0 +1,11 @@
+(ns user
+  (:require [datomic.client.api :as d]))
+
+(def cfg {:server-type :ion
+          :region "us-east-1" ;; e.g. us-east-1
+          :system "bud"
+          :endpoint "http://entry.bud.us-east-1.datomic.net:8182/"
+          :proxy-port 8182})
+
+(defn get-client []
+  (d/client cfg))
